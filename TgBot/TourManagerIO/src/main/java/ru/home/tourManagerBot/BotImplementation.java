667ceab65c629bf4da49a57d3300bad9fb1aaad7
Gennadii Koshelev <temniipushistik@gmail.com>
2021-11-22 21:42:52 +0300

    recom_start with new Token

--- TgBot/TourManagerIO/src/main/java/ru/home/tourManagerBot/BotImplementation.java
+++ TgBot/TourManagerIO/src/main/java/ru/home/tourManagerBot/BotImplementation.java
@@ -8,7 +8,8 @@ import ru.home.tourManagerBot.commands.*;
 import java.util.HashMap;
 
 public class BotImplementation extends TelegramLongPollingBot {
-    private static final String TOKEN = "2067787448:AAEsIUyOxkUGdB4SrRn0aJHprr3IsjzwUOk";
+  // old private static final String TOKEN = "2067787448:AAEsIUyOxkUGdB4SrRn0aJHprr3IsjzwUOk";
+    private static final String TOKEN = "";
     private static final String USERNAME = "Tour_ManagerBot";
     //private String userName;
     private String managerName;
@@ -65,7 +66,7 @@ public class BotImplementation extends TelegramLongPollingBot {
     //создаем мапу для хранения полученных данных. В стринг - @имя, массив эррея - данные об этом
     //пользователе
     public static HashMap<String, HashMap<String, String>> userState = new HashMap<>();
-    public static HashMap<String,String> mainClientBD = new HashMap<>();
+    public static HashMap<String, String> mainClientBD = new HashMap<>();
 
 
     public long getChat_id() {
@@ -95,23 +96,9 @@ public class BotImplementation extends TelegramLongPollingBot {
             try {
                 if (text.equals("/start")) {
                     execute(new Start().run(update));
+                }
 //если нажато добавить пользователя или в хэшмапе есть значение и в этом значении(эррейлисте) первое значение "добавить пользователя"
-                } else if (text.equals("Добавить пользователя") || (create == true)) {
-                    //выводит сообщение введите нужный емейл:
-                    execute(new CreateUser2().run(update));
 
-                } else if (text.equals("Редактировать пользователя") || (change ==true)) {
-                    execute(new ChangeUser().run(update));
-
-                }else if (text.equals("Получить пользователя")|| (obtain ==true)){
-                    execute((new ObtainUser().run(update)));
-                }
-                else if (text.equals("Получить всех пользователей")){
-                    execute((new ObtainAll().run(update)));
-                }
-                else if (text.equals("Удалить пользователя")||delete==true){
-                    execute((new DeleteUser().run(update)));
-                }
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
